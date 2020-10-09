f80d6c7f12fc5c35b10dd4dc0074563108a989a2
TerryLiu18 <pvntr@outlook.com>
2020-10-10 00:37:32 +0800

    delete unused in pre_process/

---
+++ pre_process/pre_process.bat
@@ -0,0 +1,8 @@
+start /wait python utils.py
+start /wait python user_profile_processing.py
+start /wait python tweet_profile_processing.py
+start /wait python tree_processing.py
+start /wait python words_mapping.py
+start /wait python build_graph_connection.py
+
+
